5a461d7b321df5f2b32f9bf9ea54506ad364e41e
anastasiya <burykh_2000@mail.ru>
2023-03-14 14:35:23 +0300

    add tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,4 @@
-from main import Channel
+from main import Channel, Video, PLVideo
 import pytest
 
 
@@ -11,12 +11,27 @@ def ch1():
 def ch2():
     return Channel('UC1eFXmJNkjITxPFWTy6RsWg')
 
+@pytest.fixture
+def video1():
+    return Video('9lO06Zxhu88')
+
+@pytest.fixture
+def video2():
+    return PLVideo('BBotskuyw_M', 'PL7Ntiz7eTKwrqmApjln9u4ItzhDLRtPuD')
 
 def test_str(ch1):
     assert ch1.__str__() == 'Youtube-канал: вДудь'
 
 def test_add(ch1, ch2):
-    assert ch1.__add__(ch2) == 14000000
+    assert ch1.__add__(ch2) == 14010000
 
 def test_lt(ch1, ch2):
     assert ch1.__lt__(ch2) is False
+
+
+def test_str(video1):
+    assert video1.__str__() == 'Как устроена IT-столица мира / Russian Silicon Valley (English subs)'
+
+
+def test_str(video2):
+    assert video2.__str__() == 'Пушкин: наше все? (Литература)'
